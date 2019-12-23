d6cfd4379897958ea0f3b6080516d4770c83b50b
Chev <chev@chevtek.io>
2019-12-23 00:45:26 -0700

    wip

--- generators/full-stack/index.ts
+++ generators/full-stack/index.ts
@@ -1,3 +1,12 @@
+import { exec } from "child_process";
+
 export default () => {
   console.log("Generator running...");
+  exec('echo "hi"', (error: any, stdout: any, stderr: any) => {
+    stdout.pipe(process.stdout);
+    stderr.pipe(process.stderr);
+    if (error !== null) {
+      console.log(`exec error: ${error}`);
+    }
+  });
 };
